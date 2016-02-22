58cdb425941496decb9fc10fdcb10630febef20f
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 11:44:31 +0200

    mass

---
+++ src/epam/com/Main.java
@@ -0,0 +1,19 @@
+package epam.com;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 8;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
